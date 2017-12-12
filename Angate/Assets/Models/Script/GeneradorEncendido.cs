f4a5701b1071a121cae03b4ebeb11397cde603ab
mutuerz <jelga13@gmail.com>
2017-12-12 15:38:31 -0500

    Start Point (after fixing gitnore)
    Here are the basic estructure of the game.
    Assets: Some textures, a couple of models (lamp and a generator), I have build a really small maze like lab and I have implemented a FPSController in where I put a raycast coming from the camera to detect objects.
    Finally a from the camera scrip we can call other functions for the actions of interactive objects, for example now I have an generator that powers up light  .

---
+++ Angate/Assets/Models/Script/GeneradorEncendido.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorEncendido : MonoBehaviour {
+   public GameObject WhiteLight;
+   public GameObject EmergencyLight;
+   public GameObject DarkScreen;
+	
+	
+    public void EncenderLuces()
+    {
+        WhiteLight.SetActive(true);
+        EmergencyLight.SetActive(false);
+        DarkScreen.SetActive(false);
+    }
+}
---
+++ Angate/Assets/Models/Script/Interaccion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Interaccion : MonoBehaviour {
+    public Image Mira;
+    bool contacto = false;
+    string Objetcname;//nombre del objeto que se tiene seleccionado
+    public GameObject Generator;// objeto interactuable
+    GeneradorEncendido Encender;
+
+
+
+    void Start () {
+        Encender = Generator.GetComponent<GeneradorEncendido>();
+    }
+
+
+    void Update()
+    {
+       
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1f))
+        { 
+           
+            if (hit.collider.tag.Equals("Interactuable")) 
+            {
+                contacto = true;// indica que se esta seleccionando un objeto interactuable
+                Objetcname = hit.collider.name;// //Se le asigna el string del nombre del objeto que se tiene seleccionado
+                print((hit.collider.name));
+                Mira.GetComponent<Image>().color = Color.green; // Cambiar tonalidad de la mira para indicar que esta colisionando con un objeto interactuable
+            }
+            else
+            {
+                contacto = false;// no se esta seleccionando un objeto interactuable 
+            }
+        }
+        else
+        {
+            contacto = false;// no se esta seleccionando un objeto interactuable 
+            Mira.GetComponent<Image>().color = Color.white; // Reseteo del color de la mira
+        }
+        if (contacto)
+        {
+            Encender.EncenderLuces();//Enciende las luces
+        }
+        Debug.DrawRay(transform.position, transform.forward * 1f, Color.green); // dibujo de raycast como prueba
+
+    }
+
+
+
+}
